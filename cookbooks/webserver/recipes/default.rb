e70d476706b4a7fa0413d6d1d3afb8d5a66e2955
root <root@ip-172-31-8-230.ap-south-1.compute.internal>
2017-03-17 04:21:43 -0400

    addg

---
+++ cookbooks/webserver/recipes/default.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook Name:: webserver
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "httpd"
+
+service "httpd" do
+	action :start
+end
